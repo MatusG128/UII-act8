0c9d97dfc37c98f677053c834fc7171f613edac6
VANYA_ANETTE_MATUS_GAMBOA <a.21308051280380@cbtis128.edu.mx>
2024-04-26 18:31:50 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
       title: 'Flutter Hello World',
       // Application theme data, you can set the colors for the application as
@@ -17,27 +18,56 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Vanya Matus 0380'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        appBar: AppBar(
+          // The title text which will be shown on the action bar
+          title: Text(title),
         ),
-      ),
-    );
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print("Elevated Button Pressed!");
+                  },
+                  child: Icon(Icons.home),
+                  style: ElevatedButton.styleFrom(
+                      onPrimary: Color(0xff1b1b1b),
+                      primary: Color(0xffea8ccb),
+                      textStyle: TextStyle(fontSize: 25))),
+              OutlinedButton(
+                  onPressed: () {
+                    print("Elevated Button Pressed!");
+                  },
+                  child: Icon(Icons.favorite),
+                  style: ElevatedButton.styleFrom(
+                      onPrimary: Color(0xff1b1b1b),
+                      primary: Color(0xffe1d77a),
+                      textStyle: TextStyle(fontSize: 25))),
+              TextButton(
+                  onPressed: () {
+                    print("Elevated Button Pressed!");
+                  },
+                  child: Icon(Icons.mood),
+                  style: ElevatedButton.styleFrom(
+                      onPrimary: Color(0xff1b1b1b),
+                      primary: Color(0xff52657e),
+                      textStyle: TextStyle(fontSize: 25))),
+            ],
+          ),
+        ));
   }
 }
